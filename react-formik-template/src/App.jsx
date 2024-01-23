72f60fb85c36ea60b9b813ffeda6b027dea8c8a5
Ross Matthew Santos <matthewsantos120216@gmail.com>
2024-01-23 15:51:49 +0800

    added formik feature and form design

--- react-formik-template/src/App.jsx
+++ react-formik-template/src/App.jsx
@@ -1,34 +1,66 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import Img from '../src/assets/formImg.jpg'
+import { useFormik } from 'formik'
+
+const validate = values => {
+  const errors = {};
+  if (!values.userName) {
+    errors.userName = '';
+  } else if (values.userName.length > 15) {
+    errors.userName = 'Must be 15 characters or less';
+  }
+
+  if (!values.password) {
+    errors.password = '';
+  } else if (values.password.length > 20) {
+    errors.password = 'Must be 20 characters or less';
+  }
+
+  return errors;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const formik = useFormik({
+    initialValues: {
+      userName: '',
+      password: '',
+    },
+    validate,
+  });
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <div className='flex justify-center items-center h-screen bg-slate-400'>
+        <div className='flex h-[500px] lg:h-[500px] md:h-[500px] shadow-2xl'>
+          <form onSubmit={formik.handleSubmit} className='flex flex-col bg-gradient-to-b from-gray-900 to-gray-600 bg-gradient-to-r w-[400px] lg:w-[450px] md:w-[350px] text-center items-center py-12 rounded-xl md:rounded-r-none'>
+              <h1 className='mb-16 mt-16 md:mt-12 text-white text-3xl md:text-xl lg:text-3xl font-bold'>Sample Formik Form</h1>
+              <div className='flex justify-start flex-col items-center gap-10'>
+                <div className='flex-col flex'>
+                  <div className='flex items-center gap-4'>
+                    <span className='text-white'>Username: </span>
+                    <input onChange={formik.handleChange} value={formik.values.userName} id="userName" name="userName" type='text' className='px-2 py-1 rounded-sm'/>
+                  </div>
+                  <div className='ml-[90px] mt-2'>{formik.errors.userName ? <div className='text-red-500'>{formik.errors.userName}</div> : null}</div>
+                </div>
+                <div className='flex-col flex mb-16'>
+                  <div className='flex items-center gap-4'>
+                    <span className='text-white mr-1.5'>Password: </span>
+                    <input onChange={formik.handleChange} value={formik.values.password} id="password" name="password" type='text' className='px-2 py-1 rounded-sm'/>
+                  </div>
+                  <div>{formik.errors.password ? <div className='text-red-500'>{formik.errors.password}</div> : null}</div>
+                </div>
+              </div>
+              <div className='items-center text-center'>
+                <button type='submit' className='bg-white py-2 px-20 rounded-md font-semibold hover:bg-slate-500 hover:text-white'>Log in</button>
+              </div>
+          </form>
+          <div>
+            <img src={Img} alt='formImg' className='h-full w-[350px] rounded-r-2xl lg:block hidden md:block'/>
+          </div>
+       </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
